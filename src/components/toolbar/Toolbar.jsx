4d7d2ef75ca04311074f5d467d8d487b967f4040
carl hand <carl.a.hand92@gmail.com>
2019-08-17 06:44:54 +0100

    created Toolbar component

---
+++ src/components/toolbar/Toolbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Toolbar = (props) => (
+  <header>
+    <nav>
+      <div></div>
+      <div><a href="/">The Logo</a></div>
+      <div>
+        <ul>
+          <li><a href="/">Products</a></li>
+          <li><a href="/">Users</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+)
