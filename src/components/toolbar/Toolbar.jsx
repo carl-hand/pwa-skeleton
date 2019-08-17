59b28527f8846a41fc6a7b6ec8933137cb3d9543
carl hand <carl.a.hand92@gmail.com>
2019-08-17 06:57:54 +0100

    setup prettier

--- src/components/toolbar/Toolbar.jsx
+++ src/components/toolbar/Toolbar.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React from 'react'
+import { css } from 'emotion'
 
-export const Toolbar = (props) => (
-  <header>
-    <nav>
-      <div></div>
-      <div><a href="/">The Logo</a></div>
-      <div>
-        <ul>
-          <li><a href="/">Products</a></li>
-          <li><a href="/">Users</a></li>
-        </ul>
-      </div>
-    </nav>
-  </header>
+const toolbarHeaderCss = css`
+    width: 100%;
+    position: fixed;
+    background-color: #521751;
+`
+
+const toolbarNavigationCss = css``
+
+const toolbarLogoCss = css``
+
+const toolbarNavItemsCss = css``
+
+export const Toolbar = props => (
+    <header className={toolbarHeaderCss}>
+        <nav className={toolbarNavigationCss}>
+            <div />
+            <div className={toolbarLogoCss}>
+                <a href="/">The Logo</a>
+            </div>
+            <div className={toolbarNavItemsCss}>
+                <ul>
+                    <li>
+                        <a href="/">Products</a>
+                    </li>
+                    <li>
+                        <a href="/">Users</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
 )
